83d105eb39f625f6fca7c9e447d4b0be20855145
Renshi Seña <renshi0226@gmail.com>
2025-11-06 18:51:42 +0800

    add lualine to nvim

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'tomorrow_night', -- 👈 any built-in theme name here
+        icons_enabled = true,
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      extensions = { 'neo-tree' },
+    }
+  end,
+}
